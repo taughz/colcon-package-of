0c044a1e1d3f2db028fd91406524a3e59863c67e
Tim Perkins <code@taughz.dev>
2024-11-22 23:27:38 -0500

    Initial commmit from template

---
+++ colcon_package_of/__init__.py
@@ -0,0 +1,4 @@
+# Copyright 2024 Tim Perkins
+# Licensed under the Apache License, Version 2.0
+
+__version__ = "0.0.1"
---
+++ setup.py
@@ -0,0 +1,6 @@
+# Copyright 2024 Tim Perkins
+# Licensed under the Apache License, Version 2.0
+
+from setuptools import setup
+
+setup()
